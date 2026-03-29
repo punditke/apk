9b72344139ed12a3a62ebf92e29cce8d8d205876
Code With Pundit <codewithpundit@gmail.com>
2026-03-29 16:04:41 +0300

    Update ProfileAddEditScreen.kt

--- app/src/main/java/com/xtremeiptv/ui/profile/ProfileAddEditScreen.kt
+++ app/src/main/java/com/xtremeiptv/ui/profile/ProfileAddEditScreen.kt
@@ -2,6 +2,8 @@ package com.xtremeiptv.ui.profile
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -9,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.xtremeiptv.data.database.entity.Profile
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
