db43962b4523df5dae15668a8f74e7a850b362a1
Code With Pundit <codewithpundit@gmail.com>
2026-03-29 15:33:32 +0300

    Add Constants object with configuration values

---
+++ app/src/main/java/com/xtremeiptv/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.xtremeiptv.utils
+
+object Constants {
+    const val MAX_PROFILES = 10
+    const val DEFAULT_BUFFER_MS = 5000
+    const val EPG_CACHE_HOURS = 24
+    const val CONNECTION_TIMEOUT_SEC = 10
+    const val READ_TIMEOUT_SEC = 10
+}
