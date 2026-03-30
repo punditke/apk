d7c666b00ba43a09d087d56d7d1143331e6c0eb0
Code With Pundit <codewithpundit@gmail.com>
2026-03-30 22:13:53 +0300

    Update AccountInfoScreen.kt

--- app/src/main/java/com/xtremeiptv/ui/account/AccountInfoScreen.kt
+++ app/src/main/java/com/xtremeiptv/ui/account/AccountInfoScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
