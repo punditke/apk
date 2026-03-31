f92f70ad0e6b4079424dfff5fe8392d43609d0a5
punditke <121955010+punditke@users.noreply.github.com>
2026-03-31 12:21:32 +0300

    Refactor PlayerActivity layout imports

--- app/src/main/java/com/xtremeiptv/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/xtremeiptv/ui/player/PlayerActivity.kt
@@ -7,7 +7,8 @@ import android.view.KeyEvent
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
