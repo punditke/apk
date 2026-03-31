ab3fe0975dd7175dfc018c0be7dd524370c50c16
punditke <121955010+punditke@users.noreply.github.com>
2026-03-31 17:50:57 +0300

    Update ContentRepository.kt

--- app/src/main/java/com/xtremeiptv/data/repository/ContentRepository.kt
+++ app/src/main/java/com/xtremeiptv/data/repository/ContentRepository.kt
@@ -10,7 +10,6 @@ import com.xtremeiptv.data.network.model.VodItem
 import com.xtremeiptv.data.network.protocol.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.decodeFromString
